938e48dcb129f4ff0586e727c5592de4fb6db828
Fabio Picheli <contato@fabiopicheli.com>
2017-02-20 17:16:32 -0300

    temporary solutions

--- app/main.js
+++ app/main.js
@@ -1,1 +1,14 @@
-console.log("I'm alive!!");
+var URL_SERVER = 'https://localhost:443';
+var socket = io.connect(URL_SERVER);
+
+socket.on('open', function (data) {
+  console.log(data);
+  openInNewTab(data.url);
+});
+
+// socket.emit('open', { url: 'http://tecmundo.com.br' });
+
+function openInNewTab(url) {
+  var win = window.open(url, '_blank');
+  win.focus();
+}
